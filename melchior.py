883c407d5d5c4b185e960082207e8729758cb08a
Edmund Huber <edmund.pgh@gmail.com>
2011-11-11 07:44:06 -0800

    add support for non-ssl connections

--- melchior.py
+++ melchior.py
@@ -3,32 +3,37 @@ import socket, ssl, subprocess
 import time
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--host')
-parser.add_argument('--port')
-parser.add_argument('--password')
-parser.add_argument('--channel')
-parser.add_argument('--watch')
+parser.add_argument('--host', required=True)
+parser.add_argument('--channel', required=True)
+parser.add_argument('--port', required=False)
+parser.add_argument('--ssl', required=False, action='store_true')
+parser.add_argument('--password', required=False)
+parser.add_argument('--watch', required=False)
 args = parser.parse_args()
 assert set(['host', 'port', 'password', 'channel']) <= set(vars(args).keys())
 
-ssl_sock = ssl.wrap_socket(socket.socket())
-ssl_sock.connect((args.host, int(args.port)))
-ssl_sock.setblocking(0)
+sock = socket.socket()
+if args.ssl:
+    sock = ssl.wrap_socket(sock)
+sock.connect((args.host, int(args.port) if args.port else 6667))
+sock.setblocking(0)
 
-ssl_sock.sendall('''PASS %s
-NICK melchior
-USER melchior melchiorbot %s bla :Melchior
-''' % (args.password, args.host))
+# Register
+if args.password:
+    sock.sendall('PASS %s\r\n' % args.password)
+sock.sendall('''NICK melchiorzzz9393\r
+USER melchior33zz melchiorbot33zz %s bla :Melchior33zz\r
+''' % (args.host))
 
 last_watch_time = time.time()
 last_watch_outp = None
 read_buffer = ''
 while True:
-    time.sleep(1)
+    time.sleep(0.25)
 
     # Pick out the completed lines
     try:
-        read_buffer += ssl_sock.recv()
+        read_buffer += sock.recv(4096)
         i = read_buffer.rfind('\n')
         if i == -1:
             lines = []
@@ -44,10 +49,10 @@ while True:
                 if parts[0].startswith(':'):
                     parts = parts[1:]
                 if parts[0] == 'PING':
-                    ssl_sock.sendall('PONG %s\n' % parts[1])
+                    sock.sendall('PONG %s\r\n' % parts[1])
                 elif parts[0] == 'MODE':
                     # Cannot join til after given mode.
-                    ssl_sock.sendall('JOIN #%s\n' % args.channel)
+                    sock.sendall('JOIN #%s\r\n' % args.channel)
     except:
         # nonblocking i/o raises an exception when there's nothing to be read
         pass
@@ -60,7 +65,7 @@ while True:
             outp = subprocess.check_output(args.watch, shell=True)
             if outp != last_watch_outp:
                 for line in outp.split('\n'):
-                    ssl_sock.sendall('PRIVMSG #%s :%s\n' % (args.channel, line))
+                    sock.sendall('PRIVMSG #%s :%s\n' % (args.channel, line))
                 last_watch_outp = outp
         except:
             pass
